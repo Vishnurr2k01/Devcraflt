8e25c11ffafc873a554b94a22447ae9e5766574d
Vishnurr2k01 <vishnurr2k01@gmail.com>
2023-03-12 13:12:21 +0530

    dash

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -2,20 +2,27 @@ import React from 'react'
 
 function Profile() {
   return (
-    <div>
-        <div className='h-32 w-32 rounded-full bg-gray-400'>
+    <div style={{
+        background:'linear-gradient(180deg, #022F43 24.27%, #46737F 179.61%)'
+    }} className='flex gap-12 flex-col items-center md:flex-row w-full text-white rounded-lg p-6'>
+        <div className='h-32  w-32 rounded-full bg-gray-400'>
 
         </div>
         <div>
             <div>
-            <h1 className='text-2xl font-bold'>Name</h1>
-            <p>
+            <h1 className='text-lg lg:text-2xl text-[#FFCD4A] font-bold'>Name</h1>
+            <p className='text-sm lg:text-md font-semibold'>
                 about the profession
             </p>
             </div>
-            <div>
+            <div  className='flex mt-6 gap-6 justify-between'>
                 <div>
-                    <h3>Mail</h3>
+                    <h3 className='text-[#C2C2C2] text-base '>Mail</h3>
+                    <p>abc@gmail.com</p>
+                </div>
+                <div>
+                    <h3 className='text-[#C2C2C2] text-base '>Ph No</h3>
+                    <p>1234567</p>
                 </div>
             </div>
 
--- frontend/src/pages/Dasboard.js
+++ frontend/src/pages/Dasboard.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Profile from '../components/Profile'
-
+import Income from '../components/Income'
+import ExpenseChart from '../components/ExpenseChart'
 function Dasboard() {
+  const [selec,setselec] = useState(false)
+ const Data = [
+    {
+      id:'Income',
+     cost:50000,  
+    },
+    {
+      id:'Expense',
+      cost:24000,  
+    },
+    
+]
   return (
     <div style={{
 background:`url('/bg.png')`,
@@ -9,8 +22,35 @@ backgroundSize:'cover',
 backgroundPosition:'center',
 backgroundRepeat:'no-repeat',
 
-    }} className='text-xl min-h-screen max-w-screen'>
+    }} className='text-xl h-fit pt-12 min-h-screen max-w-screen flex flex-col md:flex-row gap-6 justify-around px-6'>
+      
+      <div className='w-full md:w-2/3 lg:w-1/3'>
+        <div className='mb-12'>
+        <h3 className='text-white text-3xl font-light'>Welcome</h3>
+        <h2 className='text-black text-5xl font-semibold'>Dasboard</h2>
+
+        </div>
       <Profile/>
+      <div style={{
+        border:'1px solid rgba(255, 255, 255, 0.5)'
+
+      }} className='bg-[#022F43] p-12 mt-12 rounded-xl h-[60vh]'>
+        <div className='flex  text-center justify-center mb-12'>
+          <div className={`${selec ? 'text-white bg-[#FFCD4A] border-[1px] p-4 border-[#FFCD4A]' : 'text-[#FFCD4A]  p-4 border-[1px] border-white'} px-8 rounded-l-xl text-2xl font-semibold`}>Income</div>
+          <div className={`${!selec ? 'text-white bg-[#FFCD4A] border-[1px] p-4  border-[#FFCD4A]' : 'text-[#FFCD4A]  p-4 border-[1px] border-white'} px-8 rounded-r-xl text-2xl font-semibold`}>Expenses</div>
+        </div>
+      <Income/>
+      </div>
+      </div>
+      <div className=' w-full md:w-1/3 bg-[#022F43] flex flex-col items-center justify-center h-fit py-12'>
+      <h3 className='mb-12 text-3xl text-white font-semibold'>Monthly Expense Tracker</h3>
+      <div className='w-80 h-80 rounded-full bg-gray-200  '>
+
+
+
+      </div>
+{/* <ExpenseChart chartData={Data}/> */}
+      </div>
     </div>
   )
 }
