4df453f219d5a31af0f490cd252fdc91d7010b48
Vishnurr2k01 <vishnurr2k01@gmail.com>
2023-03-12 13:32:13 +0530

    .

--- frontend/src/pages/Dasboard.js
+++ frontend/src/pages/Dasboard.js
@@ -2,8 +2,10 @@ import React, { useState } from 'react'
 import Profile from '../components/Profile'
 import Income from '../components/Income'
 import ExpenseChart from '../components/ExpenseChart'
+import {BiPlus} from 'react-icons/bi'
 function Dasboard() {
   const [selec,setselec] = useState(false)
+  const [open,setOpen] = useState(false)
  const Data = [
     {
       id:'Income',
@@ -16,6 +18,7 @@ function Dasboard() {
     
 ]
   return (
+    <>
     <div style={{
 background:`url('/bg.png')`,
 backgroundSize:'cover',
@@ -42,16 +45,33 @@ backgroundRepeat:'no-repeat',
       <Income/>
       </div>
       </div>
-      <div className=' w-full md:w-1/3 bg-[#022F43] flex flex-col items-center justify-center h-fit py-12'>
+      <div className=' w-full md:mt-32 md:w-1/3 bg-[#022F43] flex flex-col items-center justify-center h-fit py-12 rounded-xl'>
       <h3 className='mb-12 text-3xl text-white font-semibold'>Monthly Expense Tracker</h3>
       <div className='w-80 h-80 rounded-full bg-gray-200  '>
-
-
-
       </div>
 {/* <ExpenseChart chartData={Data}/> */}
       </div>
+
+      <div className='absolute bottom-20 right-20'>
+      <BiPlus onClick={()=>setOpen(!open)}  className='text-7xl bg-[#FFCD4A] rounded-full '/>
+      </div>
+      {open && <Open/>}
     </div>
+    </>
+  )
+}
+const Open=()=>{
+  return(
+<div className='absolute bottom-44 right-20 ' >
+  <div className='bg-white px-4 py-2 rounded-md hover:bg-slate-200 cursor-pointer'>
+    Add Expense
+  </div>
+  <div className='bg-white px-4 mt-4 py-2 rounded-md hover:bg-slate-200 cursor-pointer'>
+    Add Income
+  </div>
+
+</div>
+
   )
 }
 
