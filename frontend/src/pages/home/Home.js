6218f39141b46998146673536297086ca35d4c9a
jithin-j <jithinjagadeesh1@gmail.com>
2023-03-12 14:18:28 +0530

    home final

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -32,14 +32,22 @@ const Home = () => {
       <div className="home-top">
         <div className="home-top-nav">
           <img className="logo" src={Logo} alt="logo" />
-          <img src={Profile} alt="Profile" />
+          <div style={{marginTop:"1rem"}}>
+            <a class="fancy" href="/signup">
+              <span class="top-key"></span>
+              <span class="text">Sign Up</span>
+              <span class="bottom-key-1"></span>
+              <span class="bottom-key-2"></span>
+            </a>
+          </div>
         </div>
         <div className="home-top-data">
           <div className="home-top-detail">
             <div className="home-top-left">
               <h2 className="home-motto">Mastering Your Money</h2>
               <p className="home-motto-desc">
-                In addition to straightforward expense management, comprehensive financial management in right way{" "}
+                In addition to straightforward expense management, comprehensive
+                financial management in right way{" "}
               </p>
             </div>
             <div className="home-top-right">
@@ -55,33 +63,43 @@ const Home = () => {
               ></lottie-player>
             </div>
           </div>
-            <lottie-player src="https://lottie.host/e18325bd-d4af-4f98-9a40-2ec4b418f2f6/Ojq7zq9uKl.json" background="transparent" speed="1" style={{width: "900px", height: "500px"}} loop autoplay></lottie-player>
+          <lottie-player
+            src="https://lottie.host/e18325bd-d4af-4f98-9a40-2ec4b418f2f6/Ojq7zq9uKl.json"
+            background="transparent"
+            speed="1"
+            style={{ width: "900px", height: "500px" }}
+            loop
+            autoplay
+          ></lottie-player>
         </div>
       </div>
 
       {/* home top part ends */}
       {/* home goals part begins */}
 
-        <div className="home-mid">
-          <div className="home-mid-left">
-            <img src={coin} alt="coin" />
-            <h1>EXPENSE <br/>TRACKER</h1>
-          </div>
-          <div className="home-mid-right">
-            <div className="home-goals">Monitoring Spending</div>
-            <div className="home-goals">Budgeting</div>
-            <div className="home-goals">Identifying Areas of Overspending</div>
-            <div className="home-goals">Saving Money</div>
-          </div>
+      <div className="home-mid">
+        <div className="home-mid-left">
+          <img src={coin} alt="coin" />
+          <h1>
+            EXPENSE <br />
+            TRACKER
+          </h1>
         </div>
+        <div className="home-mid-right">
+          <div className="home-goals">Monitoring Spending</div>
+          <div className="home-goals">Budgeting</div>
+          <div className="home-goals">Identifying Areas of Overspending</div>
+          <div className="home-goals">Saving Money</div>
+        </div>
+      </div>
       {/* home goals ends */}
       {/* expense chart begins */}
-        <div className="expense-chart">
-          <div className="piechart">
-            <h1 className="expense-chart-title"> Expense Report</h1>
-            <ExpenseChart chartData={userData} />
-          </div>
+      <div className="expense-chart">
+        <div className="piechart">
+          <h1 className="expense-chart-title"> Expense Report</h1>
+          <ExpenseChart chartData={userData} />
         </div>
+      </div>
       {/* expense chart ends */}
       {/* bottom home starts */}
       <div className="about" data-aos="fade-right">
@@ -89,15 +107,16 @@ const Home = () => {
           <img src={tax} alt="reward" />
           <div className="about-desc">
             <h1>Tax Purposes</h1>
-            <h3>Determine deductible expenses and can simplify the tax preparation process.</h3>
+            <h3>
+              Determine deductible expenses and can simplify the tax preparation
+              process.
+            </h3>
           </div>
         </div>
         <div className="about-row">
           <div className="about-desc">
             <h1>Financial Planning</h1>
-            <h3>
-              Employ our assistance to plan for future financial goals
-            </h3>
+            <h3>Employ our assistance to plan for future financial goals</h3>
           </div>
           <img src={goals} alt="goals" />
         </div>
